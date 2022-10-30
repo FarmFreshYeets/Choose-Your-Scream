1926da81e3c3f111b255d84afd544eb00df8232f
Camden Van Sciver <camdenvs02@gmail.com>
2022-10-30 11:21:23 -0700

    added search button functionality with imdb api

--- assets/js/results.js
+++ assets/js/results.js
@@ -1,5 +1,6 @@
 var edamamApiUrl = 'https://api.edamam.com/api/recipes/v2?type=public&q='
 var foodKeyword = $('#food-keyword') // '#food-keyword' does not currently exist
+var imdbRequestUrl = localStorage.getItem('queryUrl')
 
 function foodSearch(searchKeyword) {
     var requestUrl = edamamApiUrl + searchKeyword + '&app_id=ec3b9d99&app_key=5c1cf51a327c141221f933d66d93d0d6'
@@ -12,3 +13,23 @@ function foodSearch(searchKeyword) {
             console.log(data)
         })
 }
+
+function getApi(x) {
+    fetch(x).then(function (response) {
+        if (response.ok) {
+            response.json().then(function (data) {
+                var movie1 = Math.floor(Math.random() * 50)
+                var movie2 = Math.floor(Math.random() * 50)
+                var movie3 = Math.floor(Math.random() * 50)
+                localStorage.setItem('movie-name1', data.results[movie1].title)
+                localStorage.setItem('movie-name2', data.results[movie2].title)
+                localStorage.setItem('movie-name3', data.results[movie3].title)
+                localStorage.setItem('movie-desc1', data.results[movie1].plot)
+                localStorage.setItem('movie-desc2', data.results[movie2].plot)
+                localStorage.setItem('movie-desc3', data.results[movie3].plot)
+            })
+        }
+    })
+}
+
+getApi(imdbRequestUrl)
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,68 +1,20 @@
-// var queryUrl = "https://imdb-api.com/en/API/" + title + "/k_1234567/tt1832382";
-// var queryUrl = "https://imdb-api.com/en/API/" + title + "/k_alvf6u1h/tt1832382";
-
-// var watchMovie = $('#') // var newMovie = $('input[name="movie-input"]').val();
-// var imbdApiURL = 'https://imdb-api.com/en/API/SearchMovie/k_alvf6u1h/scream';
-
-var title;
-var movieName = document.getElementById("#movieName");
-
-function searchTitle(event) {
-    event.preventDefault();
-    var title = document.getElementById("#keyword-input").value
-    var queryUrl = 'https://imdb-api.com/en/API/SearchMovie/k_alvf6u1h/' + title;
-
-    fetch(queryUrl).then(titleResponse => {
-        console.log(titleResponse)
-        if (titleResponse.ok) {
-            titleResponse.json().then(movieData = {
-                console.log(movieData)
-                movieName.textContent = movieData.title;
-            })
-        }
-    }
-    
-    var requestOptions = {
-        method: 'GET',
-        redirect: 'follow'
-    };
-
-    fetch('https://imdb-api.com/en/API/Title/k_alvf6u1h/tt1832382', requestOptions)
-        .then(response => response.text())
-        .then(result => console.log(result))
-        .catch(error => console.log('error', error));
-
-    searchTitle()
-
-    searchButton.addEventListener("click", searchTitle);
-}
-
-var requestURL = 'https://imdb-api.com/en/API/SearchMovie/k_alvf6u1h/inception 2010';
+var requestURL = 'https://imdb-api.com/en/API/AdvancedSearch/k_alvf6u1h/';
 var redirectUrl = './result.html'
 var searchBtnEl = $('#search-btn')
 var selectAgeEl = document.querySelector('#age-selection')
 var inputKeywordEl = document.querySelector('#keyword-input')
 
-function getApi() {
-    var requestURL = ('https://imdb-api.com/en/API/SearchMovie/k_alvf6u1h/inception 2010');
-    fetch(requestUrl)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            console.log(data);
-        });
-}
-
-// for right now all this does is take us to the results page
-function handleFormSubmit(event) {
-    event.preventDefault()
+function searchTitle() {
     var selectedAge = selectAgeEl.value
     var userKeywords = inputKeywordEl.value
-    console.log(selectedAge)
-    console.log(userKeywords)
+    var queryUrl = requestURL + '?certificates=us:' + selectedAge + '&keywords=' + userKeywords;
+    localStorage.setItem('queryUrl', queryUrl)
+}
+
+function handleFormSubmit(event) {
+    event.preventDefault()
+    searchTitle()
     location.assign(redirectUrl)
 }
 
 searchBtnEl.on('click', handleFormSubmit)
-
